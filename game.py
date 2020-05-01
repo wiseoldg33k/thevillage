f5e1d2c24742835a9caf950f9acff6f914af9c17
Eric Gazoni <eric@adimian.com>
2020-05-01 17:08:24 +0200

    change grid color to a less aggressive

--- game.py
+++ game.py
@@ -1,17 +1,19 @@
-import arcade
 import os
+import random
+from collections import deque
+
+import arcade
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
-import random
 
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 1024
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1920
 SCREEN_TITLE = "The Village"
 
-VILLAGER_SPRITE_SCALING = 0.5
+VILLAGER_SPRITE_SCALING = 0.8
 BUILDING_SPRITE_SCALING = 0.5
-RESOURCE_SPRITE_SCALING = 0.5
+RESOURCE_SPRITE_SCALING = 0.8
 
 GRID_SIZE = 64
 
@@ -29,9 +31,11 @@ class Villager(arcade.Sprite):
         super().__init__(*args, **kwargs)
         self.game = game
         self.name = name
-        self.current_path = []
+        self.current_path = deque([])
 
     def compute_path(self, obj):
+        grid = Grid(matrix=self.game.matrix)
+
         grid_obj_pos = pixels_to_grid(
             obj.center_x, obj.center_y, max_y=SCREEN_HEIGHT, grid_size=GRID_SIZE
         )
@@ -39,15 +43,13 @@ class Villager(arcade.Sprite):
             self.center_x, self.center_y, max_y=SCREEN_HEIGHT, grid_size=GRID_SIZE
         )
 
-        start = self.game.grid.node(*grid_self_pos)
-        end = self.game.grid.node(*grid_obj_pos)
+        start = grid.node(*grid_self_pos)
+        end = grid.node(*grid_obj_pos)
 
         finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-        path, runs = finder.find_path(start, end, self.game.grid)
+        path, runs = finder.find_path(start, end, grid)
 
-        print(self.name.center(20, "="))
-        print(self.game.grid.grid_str(path=path, start=start, end=end))
-
+        path = deque(path)
         self.current_path = path
         return path
 
@@ -56,7 +58,14 @@ class Villager(arcade.Sprite):
             path = self.current_path
         else:
             if self.game.resources:
-                path = self.compute_path(self.game.resources[0])
+                target = random.choice(self.game.resources)
+                path = self.compute_path(target)
+
+        if path:
+            next_move = path.popleft()
+            self.center_x, self.center_y = grid_to_pixels(
+                *next_move, max_y=SCREEN_HEIGHT, grid_size=GRID_SIZE
+            )
 
 
 class Building(arcade.Sprite):
@@ -85,18 +94,7 @@ class MyGame(arcade.Window):
 
         self.town_hall_location = (0, 0)
         self.pause = False
-        self.show_pathfinding_grid = True
-        self.grid = None
-
-        self.villagers = arcade.SpriteList()
-        self.buildings = arcade.SpriteList()
-        self.resources = arcade.SpriteList()
-
-    def setup(self, rate=1 / 60):
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
-        self.pause = False
-        self.set_update_rate(rate=rate)
+        self.show_pathfinding_grid = False
 
         matrix = []
         for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
@@ -104,16 +102,24 @@ class MyGame(arcade.Window):
             for x in range(0, SCREEN_WIDTH, GRID_SIZE):
                 row.append(1)
             matrix.insert(0, row)
+        self.matrix = matrix
 
-        self.grid = Grid(matrix=matrix)
-        print(self.grid.grid_str())
+        self.villagers = arcade.SpriteList()
+        self.buildings = arcade.SpriteList()
+        self.resources = arcade.SpriteList()
+
+    def setup(self, rate=1 / 60):
+        # Set the background color
+        arcade.set_background_color(arcade.color.DIRT)
+        self.pause = False
+        self.set_update_rate(rate=rate)
 
     def draw_pathfinding_canvas(self):
         for x in range(0, SCREEN_WIDTH, GRID_SIZE):
-            arcade.draw_line(x, 0, x, SCREEN_HEIGHT, arcade.color.BLUE, 1)
+            arcade.draw_line(x, 0, x, SCREEN_HEIGHT, arcade.color.CYAN, 1)
 
         for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
-            arcade.draw_line(0, y, SCREEN_WIDTH, y, arcade.color.BLUE, 1)
+            arcade.draw_line(0, y, SCREEN_WIDTH, y, arcade.color.CYAN, 1)
 
     def on_draw(self):
         # This command has to happen before we start drawing
@@ -177,6 +183,8 @@ def main():
     game.add_town_hall(0.5, 0.5, "medievalStructure_09.png")
 
     game.add_resource(0.2, 0.2, "medievalEnvironment_19.png")
+    game.add_resource(0.8, 0.8, "medievalEnvironment_19.png")
+    game.add_resource(0.1, 0.9, "medievalEnvironment_19.png")
 
     game.add_villager("Alice", "female_idle.png")
     game.add_villager("Rob", "zombie_idle.png")
