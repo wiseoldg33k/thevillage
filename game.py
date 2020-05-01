1ebcb29c5d42ee45d3341d048c3a44631644adbb
Eric Gazoni <eric@adimian.com>
2020-05-01 15:23:31 +0200

    add pathfinding grid and resize sprites to fit in the grid

--- game.py
+++ game.py
@@ -3,19 +3,21 @@ import os
 
 import random
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 1024
 SCREEN_TITLE = "The Village"
 
 VILLAGER_SPRITE_SCALING = 0.5
-BUILDING_SPRITE_SCALING = 0.6
-RESOURCE_SPRITE_SCALING = 0.8
+BUILDING_SPRITE_SCALING = 0.5
+RESOURCE_SPRITE_SCALING = 0.5
+
+GRID_SIZE = 64
 
 
 class Villager(arcade.Sprite):
     def update(self):
-        self.center_x += random.randint(-5, 5)
-        self.center_y += random.randint(-5, 5)
+        self.center_x += random.randint(-1, 1) * GRID_SIZE
+        self.center_y += random.randint(-1, 1) * GRID_SIZE
 
 
 class Building(arcade.Sprite):
@@ -43,21 +45,32 @@ class MyGame(arcade.Window):
         os.chdir(file_path)
 
         self.town_hall_location = (0, 0)
+        self.pause = False
+        self.show_pathfinding_grid = True
 
         self.villagers = arcade.SpriteList()
         self.buildings = arcade.SpriteList()
         self.resources = arcade.SpriteList()
 
     def setup(self, rate=1 / 60):
-
         # Set the background color
         arcade.set_background_color(arcade.color.AMAZON)
-
+        self.pause = False
         self.set_update_rate(rate=rate)
+
+    def draw_pathfinding_canvas(self):
+        for x in range(0, SCREEN_WIDTH, GRID_SIZE):
+            arcade.draw_line(x, 0, x, SCREEN_HEIGHT, arcade.color.BLUE, 1)
+
+        for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
+            arcade.draw_line(0, y, SCREEN_WIDTH, y, arcade.color.BLUE, 1)
 
     def on_draw(self):
         # This command has to happen before we start drawing
         arcade.start_render()
+
+        if self.show_pathfinding_grid:
+            self.draw_pathfinding_canvas()
 
         self.buildings.draw()
         self.resources.draw()
@@ -66,7 +79,14 @@ class MyGame(arcade.Window):
         self.villagers.draw()
 
     def on_update(self, delta_time):
-        self.villagers.update()
+        if not self.pause:
+            self.villagers.update()
+
+    def on_key_release(self, key, modifier):
+        if key == arcade.key.SPACE:
+            self.pause = not self.pause
+        if key == arcade.key.P:
+            self.show_pathfinding_grid = not self.show_pathfinding_grid
 
     def add_villager(self, name, imagename):
         v = Villager(
@@ -82,8 +102,8 @@ class MyGame(arcade.Window):
             os.path.join("assets", "terrain", "Structure", imagename),
             scale=BUILDING_SPRITE_SCALING,
         )
-        b.center_x = int(x_placement * SCREEN_WIDTH)
-        b.center_y = int(y_placement * SCREEN_HEIGHT)
+        b.center_x = (int(x_placement * SCREEN_WIDTH) // GRID_SIZE) * GRID_SIZE
+        b.center_y = (int(y_placement * SCREEN_HEIGHT) // GRID_SIZE) * GRID_SIZE
         self.buildings.append(b)
 
         self.town_hall_location = (b.center_x, b.center_y)
@@ -93,15 +113,15 @@ class MyGame(arcade.Window):
             os.path.join("assets", "terrain", "Environment", imagename),
             scale=RESOURCE_SPRITE_SCALING,
         )
-        res.center_x = int(x_placement * SCREEN_WIDTH)
-        res.center_y = int(y_placement * SCREEN_HEIGHT)
+        res.center_x = (int(x_placement * SCREEN_WIDTH) // GRID_SIZE) * GRID_SIZE
+        res.center_y = (int(y_placement * SCREEN_HEIGHT) // GRID_SIZE) * GRID_SIZE
         self.resources.append(res)
 
 
 def main():
     """ Main method """
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup(1 / 12)
+    game.setup(1 / 6)
 
     game.add_town_hall(0.5, 0.5, "medievalStructure_09.png")
 
